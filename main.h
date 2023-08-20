c7d7f1176fdc40e6382d319c35ae261ca4bdae09
e-Fafali <sandyagbottah@gmail.com>
2023-08-20 11:25:45 +0000

    Initial commit

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar to print characters to standard output */
+int _putchar(char c);
+
+/* printf function */
+int _printf(const char *format, ...);
+
+
+#endif
