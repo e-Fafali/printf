aef3314307f85959966478665aa537379007b33b
e-Fafali <sandyagbottah@gmail.com>
2023-08-22 14:30:23 +0000

    first test 2

--- 3-special_xters.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "main.h"
-
-/**
- * puts_2(char)
- */
--- main.h
+++ main.h
@@ -1,13 +1,32 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stddef.h>
+
 /* _putchar to print characters to standard output */
 int _putchar(char c);
-
-/* printf function */
-int _printf(const char *format, ...);
 
 /*  */
 void _puts(char *str);
 
+/* Format specifier function */
+int _printf(const char *format, ...);
+
+/* function pointer for the format specifier */
+typedef int (*format_function)(va_list args);
+
+typedef struct {
+	char spec;
+	format_function function;
+} format_spec;
+
+/* the collector */
+format_spec get_format_func(char spec);
+
+int print_int();
+int print_str();
+int print_octal();
+int print_percent();
+int print_char();
 #endif
